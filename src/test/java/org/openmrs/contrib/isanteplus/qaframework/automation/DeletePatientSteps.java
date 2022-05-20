dddad78e33c2839d63f3e86746c1d7cc4a7994ed
HerbertYiga <herbertyiga@gmail.com>
2022-05-20 16:02:23 +0300

    fix

--- src/test/java/org/openmrs/contrib/isanteplus/qaframework/automation/DeletePatientSteps.java
+++ src/test/java/org/openmrs/contrib/isanteplus/qaframework/automation/DeletePatientSteps.java
@@ -71,7 +71,7 @@ public class DeletePatientSteps extends RemoteTestBase {
 	
 	@And("User clicks 'Delete Patient'")
 	public void userClickOnDeletePatient() {
-		dashboardPage = findPatientPage.clickOnDeletePatient();;
+		dashboardPage = findPatientPage.clickOnDeletePatient();
 	}
 	
 	@Then("Patient deleted successfully message appears, and redirected to patient search page")
