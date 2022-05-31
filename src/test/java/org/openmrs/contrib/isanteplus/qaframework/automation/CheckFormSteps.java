2ad1e57df0816ab65ee306365c297818fbe189c1
HerbertYiga <herbertyiga@gmail.com>
2022-05-31 12:25:57 +0300

    fix

--- src/test/java/org/openmrs/contrib/isanteplus/qaframework/automation/CheckFormSteps.java
+++ src/test/java/org/openmrs/contrib/isanteplus/qaframework/automation/CheckFormSteps.java
@@ -2,6 +2,8 @@ package org.openmrs.contrib.isanteplus.qaframework.automation;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
+
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
@@ -16,6 +18,7 @@ import org.openmrs.contrib.isanteplus.qaframework.automation.page.HomePage;
 import org.openmrs.contrib.isanteplus.qaframework.automation.page.LoginPage;
 import org.openmrs.contrib.isanteplus.qaframework.automation.page.PatientDashBoardPage;
 import org.openmrs.contrib.isanteplus.qaframework.automation.test.RemoteTestBase;
+import org.openmrs.contrib.isanteplus.qaframework.util.TestsUtil;
 
 public class CheckFormSteps extends RemoteTestBase {
 	
@@ -31,8 +34,17 @@ public class CheckFormSteps extends RemoteTestBase {
 	
 	private HomePage homePage;
 	
+	private String jsonData = "{\"resourceType\":\"Patient\",\"identifier\":[{\"id\":\"" + TestsUtil.generateRandomUUID() + "\",\"extension\":[{\"url\":\"http://fhir.openmrs.org/ext/patient/identifier#location\",\"valueReference\":{\"reference\":\"Location/0a2c0967-2a56-41c9-9ad5-0bd959861b42\",\"type\":\"Location\",\"display\":\"CS de la Croix-des-Bouquets\"}}],\"use\":\"usual\",\"type\":{\"text\":\"Code ST\"},\"system\":\"http://localhost:8000/openmrs/fhir2/6-code-st\",\"value\":\"" + TestsUtil.generateCodeST() + "\"}],\"active\":true,\"name\":[{\"id\":\"" + TestsUtil.generateRandomUUID() + "\",\"family\":\"Paul\",\"given\":[\"Keba\"]}],\"gender\":\"male\",\"birthDate\":\"1971-04-11\",\"deceasedBoolean\":false,\"address\":[{\"id\":\"" + TestsUtil.generateRandomUUID() + "\",\"extension\":[{\"url\":\"http://fhir.openmrs.org/ext/address\",\"extension\":[{\"url\":\"http://fhir.openmrs.org/ext/address#address1\",\"valueString\":\"Address17001\"}]}],\"use\":\"home\",\"city\":\"City7001\",\"state\":\"State7001\",\"postalCode\":\"47002\",\"country\":\"Country7001\"}]}";
+
+	private  String url = "https://iplus3.openelis-global.org/openmrs/ws/fhir2/R4/Patient/";
+
+	private String username = "admin";
+
+	private String password = "Admin123";
+	
 	@Before(RunTest.HOOK.CHECK_FORMS)
-	public void setUp() {
+	public void setUp() throws IOException {
+		TestsUtil.addPatient(url,jsonData,username,password);
 		loginPage = new LoginPage(getDriver());
 	}
 	
